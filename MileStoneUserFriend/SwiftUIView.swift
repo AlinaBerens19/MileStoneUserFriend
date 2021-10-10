316922f4a9c2f1f981cc2e45a4050bf6010b6078
Alina Berens <berens.alina019@gmail.com>
2021-10-10 12:50:11 +0300

    Test

--- MileStoneUserFriend/SwiftUIView.swift
+++ MileStoneUserFriend/SwiftUIView.swift
@@ -13,6 +13,8 @@ struct SwiftUIView: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             
         Text("Alina")
+            
+        Text("Alina")
         }
     }
 }
