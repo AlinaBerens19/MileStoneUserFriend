bec326b4c7169171e353aa157aba90dee8397f20
Alina Berens <berens.alina019@gmail.com>
2021-10-10 11:59:09 +0300

    Added new Text to SwiftUiView

--- MileStoneUserFriend/SwiftUIView.swift
+++ MileStoneUserFriend/SwiftUIView.swift
@@ -11,6 +11,8 @@ struct SwiftUIView: View {
     var body: some View {
         VStack {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+        Text("Alina")
         }
     }
 }
