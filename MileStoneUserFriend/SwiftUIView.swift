6f204304285e2ac79c8a509d525514316575502b
Alina Berens <berens.alina019@gmail.com>
2021-10-10 09:53:26 +0300

    Second commit

--- MileStoneUserFriend/SwiftUIView.swift
+++ MileStoneUserFriend/SwiftUIView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct SwiftUIView: View {
     var body: some View {
+        VStack {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
