b9a45a14ef870c27c88bf9349f9f4c0ee9876c27
Alina Berens <berens.alina019@gmail.com>
2021-10-16 12:48:22 +0300

    Commit #2. do/catch fetch request

--- MileStoneUserFriend/ContentView.swift
+++ MileStoneUserFriend/ContentView.swift
@@ -17,113 +17,16 @@ struct ContentView: View {
         NSSortDescriptor(keyPath: \Users.company, ascending: true)
     ]) var items: FetchedResults<Users>
     
-    @State private var results = [User]()
-    @State var editMode = EditMode.inactive
-    @State var selection = Set<String>()
-    
-    init() {
-        loadData()
-    }
-    
-    private var editButton: some View {
-        if editMode == .inactive {
-            return Button(action: {
-                self.editMode = .active
-                self.selection = Set<String>()
-            }) {
-                Text("Edit")
-            }
-        }
-        else {
-            return Button(action: {
-                self.editMode = .inactive
-                self.selection = Set<String>()
-            }) {
-                Text("Done")
-            }
-        }
-    }
-    
-    private var deleteButton: some View {
-        if editMode == .inactive {
-            return Button(action: {}) {
-                Image(systemName: "")
-            }
-        } else {
-            return Button(action: deleteUsers) {
-                Image(systemName: "trash")
-            }
-        }
-    }
-    
-    private func deleteUsers() {
+    @State private var users = [User]()
 
-    }
-    
     var body: some View {
         NavigationView {
-        VStack {
-            List(selection: $selection) {
-            ForEach(items, id: \.self) { user in
-                VStack(alignment: .leading) {
-                    NavigationLink(destination: DetailView(name: user.wrappedName, email: user.wrappedEmail, age: user.age, company: user.wrappedCompany, about: user.wrappedAbout, registered: user.wrappedDate, address: user.wrappedAddress, isActive: user.isActive, id: user.wrappedId)) {
-                    Text((user.name) ?? "Unknown")
-                        .font(.headline)
-                 }
-             }
-        }
-       .onDelete(perform: deleteUser)
-                
-        }
-            
-            Spacer()
-            
-            Button("Add users") {
-                saveData()
+            List(users, id: \.id) { user in
+                Text("\(user.name)")
             }
-        }
-        .navigationBarItems(leading: deleteButton, trailing: editButton)
-        .environment(\.editMode, self.$editMode)
+            .onAppear(perform: loadData)
         }
     }
-    
-    func deleteUser(at offsets: IndexSet) {
-        for offset in offsets {
-            // find this book in our fetch request
-            let user = items[offset]
-
-            // delete it from the context
-            moc.delete(user)
-        }
-
-        // save the context
-        try? moc.save()
-    }
-    
-    func saveData() {
-
-    for result in results {
-        
-            var item = Users(context: self.moc)
-            
-            item.id = result.id
-            item.about = result.about
-            item.address = result.address
-            item.company = result.company
-            item.name = result.name
-            item.age = result.age
-            item.isActive = result.isActive
-            item.registered = result.registered
-            item.email = result.email
-            
-            for friend in result.friends {
-                item.friend?.id = friend.id
-                item.friend?.name = friend.name
-            }
-            print(item)
-            try? self.moc.save()
-        }
-  }
     
     func loadData() {
             guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
@@ -132,27 +35,32 @@ struct ContentView: View {
             }
             let request = URLRequest(url: url)
 
-            URLSession.shared.dataTask(with: request) {data, response, error in
+            URLSession.shared.dataTask(with: request) { data, response, error in
                 if let data = data {
                     //pull out the declaration of the decoder object
                     let decoder = JSONDecoder()
                     //set how we want to interpret dates in the JSON
                     decoder.dateDecodingStrategy = .iso8601
                     //decode directly to an array of User structs rather than a Response
-                    if let decodedResponse = try?
-                                        decoder.decode([User].self, from: data) {
-                                        DispatchQueue.main.async {
-                                            //decodedResponse is now [User] rather than Response.User
-                                            self.results = decodedResponse
-                                        }
-                                        return
-                                    }
-                                }
-                                print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-
-                            }.resume()
-                        }
+                    do {
+                        users = try decoder.decode([User].self, from: data)
                     }
+                    catch DecodingError.keyNotFound(let key, let context) {
+                          Swift.print("could not find key \(key) in JSON: \(context.debugDescription)")
+                      } catch DecodingError.valueNotFound(let type, let context) {
+                          Swift.print("could not find type \(type) in JSON: \(context.debugDescription)")
+                      } catch DecodingError.typeMismatch(let type, let context) {
+                          Swift.print("type mismatch for type \(type) in JSON: \(context.debugDescription)")
+                      } catch DecodingError.dataCorrupted(let context) {
+                          Swift.print("data found to be corrupted in JSON: \(context.debugDescription)")
+                      } catch let error as NSError {
+                          NSLog("Error in read(from:ofType:) domain= \(error.domain), description= \(error.localizedDescription)")
+                      }
+                }
+            }.resume()
+        }
+    }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
